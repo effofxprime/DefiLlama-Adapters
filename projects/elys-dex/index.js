196eec1d729fb4761ed826e12d0d5fa1ac9198bd
Erialos <effofxprime@gmail.com>
2025-01-20 16:26:05 -0700

    Updating Elys TVL Adapter for Leverage LP TVL
    After making leverage lp's live, we have more USDC that we can track as TVL within Defi Llama.

--- projects/elys-dex/index.js
+++ projects/elys-dex/index.js
@@ -4,19 +4,45 @@ const { sumTokens2 } = require('../helper/unwrapLPs')
 async function tvl(api) {
   let key = ''
   const pools = []
+
+  // Fetch AMM Pools and process them
   do {
     const { pagination: { next_key }, pool } = await get('https://api.elys.network/elys-network/elys/amm/pool?pagination.count_total=true&pagination.per_page=1000&pagination.key=' + key)
     key = next_key
     pools.push(...pool)
   } while (key)
+
+  // Loop through pools and aggregate the token amounts
   pools.forEach(pool => {
     pool.pool_assets.forEach(({ token: { denom, amount } }) => api.add(denom, amount))
   })
 
+  // Fetch Leverage LP Pools and process positions
+  const { params } = await get('https://api.elys.network/elys-network/elys/leveragelp/params')
+  const enabledPools = params?.enabled_pools || []
+
+  let totalLeverageLpCollateral = 0
+
+  // Loop through enabled Leverage LP pools and aggregate collateral
+  for (const poolId of enabledPools) {
+    let nextKey = ''  // Pagination key for positions query
+    do {
+      const response = await get(`https://api.elys.network/elys-network/elys/leveragelp/positions-by-pool/${poolId}/${nextKey ? nextKey : ''}?pagination.count_total=true&pagination.per_page=1000`)
+      nextKey = response.pagination?.next_key || ''
+      
+      response.positions.forEach(({ position: { collateral: { amount } } }) => {
+        totalLeverageLpCollateral += parseFloat(amount)
+      })
+    } while (nextKey)
+  }
+
+  // Add the total collateral for Leverage LP pools to the API result
+  api.add('USDC', totalLeverageLpCollateral)
+
+  // Finally, return the sum of all tokens including the Leverage LP collateral
   return sumTokens2({ api })
 }
 
-
 module.exports = {
-  elys: { tvl, }
+  elys: { tvl },
 }
